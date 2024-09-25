61ded0041878e94858f3bdf254b25aa9ab9c43f5
NigelKus <nigelkusdenata2@gmail.com>
2024-09-25 19:36:30 +0700

    Update create.blade.php
    Commit testing

--- resources/views/layouts/transactional/payment_order/create.blade.php
+++ resources/views/layouts/transactional/payment_order/create.blade.php
@@ -77,6 +77,9 @@
                                     <th>Payment Requested</th>
                                     <th>Original Price</th>
                                     <th>Remaining Price</th>
+
+
+                                    
                                     <th>Action</th>
                                 </tr>
                             </thead>
