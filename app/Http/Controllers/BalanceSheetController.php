ef591e7af79c85e5b89da9023bf55be046a78664
NigelKus <nigelkusdenata2@gmail.com>
2024-10-24 17:19:19 +0700

    Balance Sheet

--- app/Http/Controllers/BalanceSheetController.php
+++ app/Http/Controllers/BalanceSheetController.php
@@ -22,15 +22,97 @@ class BalanceSheetController extends Controller
     {
         $month = $request['month'];
         $year = $request['year'];
-
-        $dateString = Carbon::createFromDate($year, $month)->endOfMonth()->toDateString(); 
-
+        $dateStringDisplay = Carbon::createFromDate($year, $month)->endOfMonth()->translatedFormat('j F Y');
         if (!checkdate($month, 1, $year)) {
             return back()->withErrors(['month' => 'Invalid month or year']);
         }
 
+        $dateStringStart = Carbon::createFromDate($year, $month)->startOfMonth()->format('Y-m-d H:i:s'); 
+        $dateStringEnd = Carbon::createFromDate($year, $month)->endOfMonth()->format('Y-m-d H:i:s'); 
 
-        return view('layouts.reports.balance_sheet.report', compact('dateString'));
+        $assetIds = ChartOfAccount::where('code', 'like', '1%')
+        ->where('status', 'active')
+        ->orderBy('code', 'asc')
+        ->pluck('id');
+
+        $totalActiva = 0;
+        $totalPasiva = 0;
+        $totalasset = [];
+
+        foreach ($assetIds as $id) {
+            $sum = Posting::where('account_id', $id)
+                ->where('amount', '<', 0)
+                ->where('date', '>=', $dateStringStart)  
+                ->where('date', '<=', $dateStringEnd) 
+                ->sum('amount');
+
+            $a = abs($sum);
+            $totalActiva = $totalActiva += $a;
+            
+            if ($sum != 0) {
+                $totalasset[$id] = [
+                    'coa' => ChartOfAccount::find($id),
+                    'total' => abs($sum),
+                ]; 
+            }
+        }
+
+        $UtangIds = ChartOfAccount::where('code', 'like', '2%')
+        ->where('status', 'active')
+        ->orderBy('code', 'asc')
+        ->pluck('id');
+
+        $totalUtang = [];
+
+        foreach ($UtangIds as $id) {
+            $sum = Posting::where('account_id', $id)
+                ->where('amount', '<', 0)
+                ->where('date', '>=', $dateStringStart)  
+                ->where('date', '<=', $dateStringEnd) 
+                ->sum('amount');
+            
+            $a = abs($sum);
+
+            $totalPasiva = $totalPasiva += $a;
+            
+            if ($sum != 0) {
+                $totalUtang[$id] = [
+                    'coa' => ChartOfAccount::find($id),
+                    'total' => abs($sum),
+                ]; 
+            }
+        }
+
+        $Modalds = ChartOfAccount::where('code', 'like', '3%')
+            ->where('status', 'active')
+            ->orderBy('code', 'asc')
+            ->pluck('id');
+
+        $codeModal = ChartOfAccount::where('code', 3000)->first();
+
+        $totalModal = Posting::where('account_id', $id)
+            ->where('amount', '>', 0)
+            ->where('date', '>=', $dateStringStart)  
+            ->where('date', '<=', $dateStringEnd) 
+            ->sum('amount');
+
+        $totalPasiva = $totalPasiva += $totalModal;
+
+        $labaId = ChartOfAccount::where('code', 4200)
+            ->pluck('id')->first();
+
+        $codeLaba = ChartOfAccount::where('code', 4200)->first();
+
+        $totalLaba = Posting::where('account_id', $labaId)
+            ->where('amount', '>', 0)
+            ->where('date', '>=', $dateStringStart)  
+            ->where('date', '<=', $dateStringEnd) 
+            ->sum('amount');
+        
+        $totalPasiva = $totalPasiva += $totalLaba;
+
+        return view('layouts.reports.balance_sheet.report', compact('dateStringDisplay', 
+        'totalasset', 'totalUtang', 'totalLaba', 'totalModal', 'codeModal', 'codeLaba', 'totalActiva', 'totalPasiva'));
     }
 
 }
--- resources/views/layouts/reports/balance_sheet/report.blade.php
+++ resources/views/layouts/reports/balance_sheet/report.blade.php
@@ -22,11 +22,83 @@
                 @csrf
 
                 <h2 class="text-left">Balance Sheet</h2>
-                <p class="text-left"><strong>{{ $dateString }}</strong></p>
+                <p class="text-left"><strong>{{ $dateStringDisplay }}</strong></p>
                 <hr>
 
-
-
+                <div class="row">
+                    <div class="col-md-6">
+                        <table class="table table-bordered" style="margin-bottom: 40px;">
+                            <thead>
+                                <tr>
+                                    <th colspan="2">Asset</th>
+                                </tr>
+                            </thead>
+                            <thead>
+                                <tr>
+                                    <th>Nama</th>
+                                    <th>Jumlah</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($totalasset as $item)
+                                <tr>
+                                    <td>{{ $item['coa']->name }}({{ $item['coa']->code }})</td>
+                                    <td>{{ number_format($item['total'], 2) }}</td>
+                                </tr>
+                            @endforeach
+                                <tr>
+                                    <td><strong>Total Asset</strong></td>
+                                    <td>{{ number_format($totalActiva, 2) }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                    <div class="col-md-6">
+                        <table class="table table-bordered" style="margin-bottom: 40px;">
+                            <thead>
+                                <tr>
+                                    <th colspan="2">Liabilites & Equity</th>
+                                </tr>
+                            </thead>
+                            <thead>
+                                <tr>
+                                    <th>Nama</th>
+                                    <th>Jumlah</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><strong>Liabilities</strong></td>
+                                    <td></td>
+                                </tr>
+                            @foreach ($totalUtang as $item)
+                                <tr>
+                                    <td>{{ $item['coa']->name }}({{ $item['coa']->code }})</td>
+                                    <td>{{ number_format($item['total'], 2) }}</td>
+                                </tr>
+                            @endforeach
+                                <tr>
+                                    <td><strong>Equity</strong></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>{{ $codeModal->name }}({{ $codeModal->code }})</td>
+                                    <td>{{ number_format($totalModal, 2) }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Laba Berjalan({{ $codeLaba->code}})</td>
+                                    <td>{{ number_format($totalLaba, 2) }}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Total Liabilities & Equity</strong></td>
+                                    <td>{{ number_format($totalPasiva, 2) }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                
                 <div class="form-group mt-3">
                     <button type="button" class="btn btn-danger" onclick="submitForm('pdf')">PDF</button>
                     <button type="button" class="btn btn-success" onclick="submitForm('excel')">Excel</button>
