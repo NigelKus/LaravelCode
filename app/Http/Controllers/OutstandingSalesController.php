e932d61ab06633bbb848ea56c4c5c5a5f555281a
NigelKus <nigelkusdenata2@gmail.com>
2024-10-28 17:06:23 +0700

    Outstanding controller

--- app/Http/Controllers/OutstandingSalesController.php
+++ app/Http/Controllers/OutstandingSalesController.php
@@ -14,11 +14,17 @@ use App\Models\JournalVoucherDetail;
 use App\Utils\AccountingEvents\AE_JM1_FinishJournalVoucher;
 use App\Utils\AccountingEvents\AE_JM2_UpdateJournalVoucher;
 use PhpOffice\PhpSpreadsheet\Chart\Layout;
+use App\Http\Controllers\OutStandingSalesController;
 
-class OutstandingSalesController extends Controller
+class OutStandingSalesController extends Controller
 {
     public function index()
     {   
         return view('layouts.reports.outstanding_sales.index');
+    }
+
+    public function outstandingOrder(Request $request)
+    {
+        dd($request->all());
     }
 }
